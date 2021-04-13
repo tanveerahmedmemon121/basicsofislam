afc6bbee412f4bc1d46f1a39482dd23395867c8f
tanveerahmedmemon121 <Tanveer.1>
2021-04-13 05:45:25 -0700

    initial

--- app/src/main/java/com/apkfeast/com/basicsofislam/Qayam.java
+++ app/src/main/java/com/apkfeast/com/basicsofislam/Qayam.java
@@ -3,12 +3,20 @@ package com.apkfeast.com.basicsofislam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Qayam extends AppCompatActivity {
 
+
+    ImageButton plasana,playbism,playfatiha,playqulhuwallah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qayam);
+
+        plasana = findViewById(R.id.plasana);
+        playbism = findViewById(R.id.playbism);
+        playfatiha = findViewById(R.id.playfatiha);
+        playqulhuwallah = findViewById(R.id.playqulhuwallah);
     }
 }
